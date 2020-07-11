416aa1c5027e13bfe166c37d99bdcf967402dcea
Sebastian <sebastiamerelor@gmail.com>
2020-07-11 05:34:13 +0200

    gameplay finished, todo: tether and balance

---
+++ Assets/Minigame1/Scripts/Asteroid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        if (hitInfo.name == "Ship")
+        {
+            hitInfo.SendMessage("SpawnFires", gameObject.transform);
+        }
+        else if(hitInfo.name == "Robot")
+        {
+            hitInfo.SendMessage("DetractPoints", gameObject.transform);
+
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Minigame1/Scripts/AsteroidShooter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidShooter : MonoBehaviour
+{
+    [SerializeField] private GameObject[] firePoints;
+    public GameObject asteroidPrefab;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Robot").transform;
+        InvokeRepeating("ShootAsteroid", 2.0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ShootAsteroid()
+    {
+        int i = Random.Range(0, 4);
+        Debug.Log(i);
+        GameObject asteroid =  Instantiate(asteroidPrefab, firePoints[i].transform.position, firePoints[i].transform.rotation);
+        Rigidbody2D rb = asteroid.GetComponent<Rigidbody2D>();
+        rb.velocity = (player.position - asteroid.transform.position).normalized * 10f;
+
+    }
+
+}
