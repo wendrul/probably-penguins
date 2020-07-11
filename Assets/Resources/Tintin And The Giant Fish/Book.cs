b25cc2455f9f99d268fec5b5837f18bccca2ab0c
Miguel <sexyexoss@gmail.com>
2020-07-11 20:35:45 +0200

    "La peche aux etoiles" Core implemented still need polishing and 5 frames

--- Assets/Resources/Tintin And The Giant Fish/Book.cs
+++ Assets/Resources/Tintin And The Giant Fish/Book.cs
@@ -35,4 +35,94 @@ public class Book : MonoBehaviour
             transform.GetChild(i).gameObject.SetActive(true);
         }
     }
+
+    public void BinbinFather()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 1;
+        page[i].gameObject.SetActive(true);
+    }
+
+    public void CryingInBed()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 2;
+        page[i].gameObject.SetActive(true);
+    }
+
+    public void Hospital()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 3;
+        page[i].gameObject.SetActive(true);
+    }
+
+    public void GranfatherFlashback()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 4;
+        page[i].gameObject.SetActive(true);
+    }
+
+    public void DreamingHappy()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 5;
+        page[i].gameObject.SetActive(true);
+    }
+
+    public void AloneInTheDark()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 6;
+        page[i].gameObject.SetActive(true);
+    }
+    public void FishGiantFish()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 7;
+        page[i].gameObject.SetActive(true);
+    }
+    public void LookGiantFish()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 8;
+        page[i].gameObject.SetActive(true);
+    }
+    public void SpeedDating()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 9;
+        page[i].gameObject.SetActive(true);
+    }
+    public void Cliff()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 10;
+        page[i].gameObject.SetActive(true);
+    }
+    public void PingHell()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 11;
+        page[i].gameObject.SetActive(true);
+    }
+    public void TenOutTen()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 12;
+        page[i].gameObject.SetActive(true);
+    }
+    public void FatherAgain()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 13;
+        page[i].gameObject.SetActive(true);
+    }
+    public void LoveNeverComesAlone()
+    {
+        page[i].gameObject.SetActive(false);
+        i = 14;
+        page[i].gameObject.SetActive(true);
+    }
 }
