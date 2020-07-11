6468792df9ec12df51b76039c079389632d9d664
Wendrul <dethomassin.etienne@gmail.com>
2020-07-11 04:34:21 +0200

    Add random approaching asteroids

---
+++ Assets/minigame0/AsteroidLauncher.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidLauncher : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private float crashDistance;
+    [SerializeField] private float minimumSpawnDelay;
+    [SerializeField] private float maximumSpawnDelay;
+    [SerializeField] private GameObject asteroidPrefab;
+
+    private List<GameObject> asteroids;
+    private float timeSinceLastAsteroid;
+    private float nextAsteroidDelay;
+
+    [SerializeField] private Transform bottomRight;
+    [SerializeField] private Transform topLeft;
+    [SerializeField] private float shieldSize;
+
+    void Start()
+    {
+        timeSinceLastAsteroid = 0;
+        nextAsteroidDelay = 0;
+        asteroids = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool normalAsteroid = true;
+        timeSinceLastAsteroid += Time.deltaTime;
+        if (normalAsteroid)
+        {
+            if (timeSinceLastAsteroid > nextAsteroidDelay)
+            {
+                timeSinceLastAsteroid = 0;
+                nextAsteroidDelay = UnityEngine.Random.Range(minimumSpawnDelay, maximumSpawnDelay);
+
+                float x = UnityEngine.Random.Range(topLeft.position.x, bottomRight.position.x);
+                float y = UnityEngine.Random.Range(topLeft.position.y, bottomRight.position.y);
+                GameObject asteroid = Instantiate(asteroidPrefab, new Vector3(x, y), asteroidPrefab.transform.rotation, this.transform);
+                asteroids.Add(asteroid);
+            }
+        }
+        CheckAsteroidCollision();
+    }
+
+    private void CheckAsteroidCollision()
+    {
+        for (int i = asteroids.Count - 1; i >= 0; i--)
+        {
+            if (asteroids[i].GetComponent<Asteroid>().distanceFromShip < crashDistance)
+            {
+                Hit(asteroids[i]);
+            }
+        }
+    }
+
+    private void Hit(GameObject asteroid)
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        float shieldDistance =  - asteroid.transform.position.magnitude;
+        
+        if (shieldDistance > shieldSize)
+        {
+            TakeDamage();
+            DestroyAsteroidFail(asteroid);
+        }
+        else
+        {
+            TallyScore();
+            DestroyAsteroidSuccess(asteroid);
+        }
+    }
+
+    private void TallyScore()
+    {
+    }
+
+    private void DestroyAsteroidSuccess(GameObject asteroid)
+    {
+        //play anim
+        Destroy(asteroid);
+        asteroids.Remove(asteroid);
+    }
+
+    private void DestroyAsteroidFail(GameObject asteroid)
+    {
+        //play anim
+        Destroy(asteroid);
+        asteroids.Remove(asteroid);
+    }
+
+    private void TakeDamage()
+    {
+    }
+}
