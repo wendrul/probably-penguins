32ac147aa3a8a81cf62545edbd4a66c3740c458a
Wendrul <dethomassin.etienne@gmail.com>
2020-07-12 18:31:57 +0200

    Place bouzgaou puzzle

--- Assets/Base Scripts/GameSwitcher.cs
+++ Assets/Base Scripts/GameSwitcher.cs
@@ -143,7 +143,7 @@ public class GameSwitcher : MonoBehaviour
                     AudioManager.Instance.PlaySFX(GameAssets.i.TransitionSfx, 0.3f);
                 }
                 // saving last minigame to handle better transition sounds
-                cameraHolder.position = cameraPositions[3].position;
+                cameraHolder.position = cameraPositions[2].position;
                 lastMiniGame = CurrentMinigame;
                 break;
             case 3:
