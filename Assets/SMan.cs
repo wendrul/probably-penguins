91f6a829c500a138ab5500953eef65efd3aa98f8
Miguel <sexyexoss@gmail.com>
2020-07-12 20:10:06 +0200

    Loading Scene

--- Assets/SMan.cs
+++ Assets/SMan.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneManager : MonoBehaviour
+public class SMan : MonoBehaviour
 {
 
     public GameObject settings;
     [SerializeField] private GameSwitcher switcher;
 
     private bool settingsActive = false;
+
     private bool ispressed;
 
 
